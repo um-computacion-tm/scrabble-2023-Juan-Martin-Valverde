36cb22dcb3baf37c3314f12315ab8b6b928a2f5f
Juan-Martin-Valverde <tintinvalverde@gmail.com>
2023-11-07 16:33:20 -0300

    no saleee

--- game/mainmenu.py
+++ game/mainmenu.py
@@ -83,18 +83,5 @@ class Main_Menu():
         else:
             print("Valor invalido por favor ingresar uno correcto.")
 
-    def Begin_game(self):
-        self.welcome_message()
-        player_count = self.get_player_count()
-        self.scrabble = ScrabbleGame(total_players=player_count)
-
-        while self.scrabble.is_playing():
-            self.scrabble.next_turn()
-            self.scrabble.start_game()
-
-            while True:
-                self.show_game_options()
-                option = int(input("Elige tus opciones: "))
-                self.handle_user_input(option)
                 
     
